e00af63ca6bad018b5e47b67b888544dbe787139
Szabados_Gergo <gergo.szabados11@gmail.com>
2025-07-04 11:31:00 +0200

    use jdbc for reserve stock

--- stock_service/src/main/java/com/order_system/stock_service/service/data/StockReserveServiceImpl.java
+++ stock_service/src/main/java/com/order_system/stock_service/service/data/StockReserveServiceImpl.java
@@ -1,12 +1,20 @@
 package com.order_system.stock_service.service.data;
 
+import com.microsoft.sqlserver.jdbc.SQLServerCallableStatement;
+import com.microsoft.sqlserver.jdbc.SQLServerDataTable;
 import com.order_system.stock_service.data.udt.StockReservationItemUDT;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,21 +25,36 @@ public class StockReserveServiceImpl implements StockReserveService {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
-    public Boolean reserveStock(Integer orderId, List<StockReservationItemUDT> values){
-        String param = values.stream()
-                .map(x -> "(" + x.getProductId() + "," + x.getQuantity() + ")")
-                .collect(Collectors.joining(","));
+    public Boolean reserveStock(Integer orderId, List<StockReservationItemUDT> values) {
+        try (Connection connection = dataSource.getConnection();
+             CallableStatement callableStatement = connection.prepareCall(
+                     "EXEC ReserveStock ?, ?, ?")) {
 
-        Query query = entityManager.createNativeQuery(
-                "DECLARE @t StockReservationItems;" +
-                "INSERT INTO @t (ProductId, Quantity) VALUES " + param + ";" +
-                "DECLARE @result BIT;" +
-                "EXEC ReserveStock ?1, @t, @result output;" +
-                "SELECT @result;");
-        query.setParameter(1, orderId);
+            SQLServerDataTable tvp = new SQLServerDataTable();
+            tvp.addColumnMetadata("ProductId", Types.INTEGER);
+            tvp.addColumnMetadata("Quantity", Types.INTEGER);
 
-        return (Boolean) query.getSingleResult();
+            for (StockReservationItemUDT item : values) {
+                tvp.addRow(item.getProductId(), item.getQuantity());
+            }
+
+            SQLServerCallableStatement sqlServerStmt = callableStatement.unwrap(SQLServerCallableStatement.class);
+            sqlServerStmt.setInt(1, orderId);
+
+            sqlServerStmt.setStructured(2, "dbo.StockReservationItems", tvp);
+
+            callableStatement.registerOutParameter(3, Types.BIT);
+
+            callableStatement.execute();
+
+            return callableStatement.getBoolean(3);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
